78741780d9864b1a1f94d3fea11b77d7d073613e
luketevl <luketevl@gmail.com>
2018-06-06 15:51:05 -0300

    Added random text in page load, fix  #15

--- src/js/translator.js
+++ src/js/translator.js
@@ -4,30 +4,36 @@ import { removeAccent } from './convert.js';
 import { debounce } from './debounce.js'; 
 
 const generateRandomKey  = (data = []) => Math.floor(Math.random() * (data.length - 0) + 0);
-const getRandomText  = (data = [], field) =>{
-  const keys = Object.keys(json);
-    field.value = keys[generateRandomKey(keys)];
-    field.dispatchEvent(new Event('keyup'));
-}
+
+const translate = (library, input) => {
+  let text = input;
+  forEach(library, (value, key) => { 
+    const regex = new RegExp(removeAccent(key), 'gm'); 
+    text = text.replace(regex, value); 
+  }); 
+  return text;
+};
 
 export const translator = (e) => {
   const $translator = document.querySelector('[data-translator="input"]');
   const $result = document.querySelector('[data-translator="result"]'); 
-  const library = json;
-  let text; 
-
+  
   const textTranslate = (e) => { 
+    let text;
     text = $translator.value.toLowerCase();  
-    forEach(library, (value, key) => { 
-      const regex = new RegExp(removeAccent(key), 'gm'); 
-      text = text.replace(regex, value); 
-    }); 
+    text = translate(json, text);
     $result.value = text || 'Tradução';  
+  };
+  
+  const getRandomText  = () => {
+    const keys = Object.keys(json);
+    const phrase = keys[generateRandomKey(keys)];
+    $translator.placeholder = phrase;
+    $result.value = translate(json, phrase);
   };
 
   $translator.addEventListener('keyup', (e) => debounce(textTranslate(), 600));
-  
-  window.addEventListener('load', () => getRandomText(json, $translator));
+  window.addEventListener('load', () => getRandomText());
 
 }; 
 
